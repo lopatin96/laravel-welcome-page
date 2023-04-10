a1f49b2f29eeebeb5f608e2235bdfc1184f0e18b
vladyslavlopatin <ilopatin3@gmail.com>
2023-04-10 23:05:28 +0200

    faq-v1

---
+++ lang/en/faq-v1.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    'upper_title' => 'upper_title',
+    'title' => 'title',
+    'contact_us' => 'contact_us',
+
+    'faq_1_question' => 'faq_1_question',
+    'faq_1_answer' => 'faq_1_answer',
+    'faq_2_question' => 'faq_2_question',
+    'faq_2_answer' => 'faq_2_answer',
+
+];
--- resources/views/welcome/faq/v1/index.blade.php
+++ resources/views/welcome/faq/v1/index.blade.php
@@ -5,28 +5,28 @@
     <div class="relative container px-4 mx-auto">
         <div class="md:max-w-4xl mx-auto">
             <p class="mb-7 text-sm text-sky-600 text-center font-semibold uppercase tracking-px">
-                {{ __('laravel-welcome-page::laravel-welcome-page.faq_upper_title') }}
+                {{ __('laravel-welcome-page::faq-v1.upper_title') }}
             </p>
             <h2 class="mb-16 text-4xl md:text-5xl xl:text-6xl text-center font-bold font-heading tracking-px-n leading-none">
-                {{ __('laravel-welcome-page::laravel-welcome-page.faq_title') }}
+                {{ __('laravel-welcome-page::faq-v1.title') }}
             </h2>
 
             <div class="mb-11 flex flex-col space-y-4">
                 @include('laravel-welcome-page::welcome.faq.v1.faq-card', [
                     'open' => true,
-                    'question' => 'laravel-welcome-page::laravel-welcome-page.faq_faq_1_question',
-                    'answer' => 'laravel-welcome-page::laravel-welcome-page.faq_faq_1_answer',
+                    'question' => 'laravel-welcome-page::faq-v1.faq_1_question',
+                    'answer' => 'laravel-welcome-page::faq-v1.faq_1_answer',
                 ])
 
                 @include('laravel-welcome-page::welcome.faq.v1.faq-card', [
                     'open' => false,
-                    'question' => 'laravel-welcome-page::laravel-welcome-page.faq_faq_2_question',
-                    'answer' => 'laravel-welcome-page::laravel-welcome-page.faq_faq_2_answer',
+                    'question' => 'laravel-welcome-page::faq-v1.faq_2_question',
+                    'answer' => 'laravel-welcome-page::faq-v1.faq_2_answer',
                 ])
             </div>
 
             <p class="text-gray-600 text-center font-medium">
-                {!! __('laravel-welcome-page::laravel-welcome-page.faq_contact_us', ['mailto' => config('mail.support')]) !!}
+                {!! __('laravel-welcome-page::faq-v1.contact_us', ['mailto' => config('mail.support')]) !!}
             </p>
         </div>
     </div>
