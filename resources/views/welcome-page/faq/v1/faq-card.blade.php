72f542d5b2bb7d4e55017fd669abeb6db225e0e5
Vladyslav Lopatin <vladyslav.lopatin@wskz.pl>
2023-04-19 15:12:31 +0200

    fix color

--- resources/views/welcome-page/faq/v1/faq-card.blade.php
+++ resources/views/welcome-page/faq/v1/faq-card.blade.php
@@ -1,4 +1,4 @@
-<details class="open:border-sky-600 border-2 rounded-2xl p-6" @if($open) open @endif>
+<details class="open:border-gray-500 border-2 rounded-2xl p-6" @if($open) open @endif>
     <summary class="mb-4 text-lg font-semibold leading-normal cursor-pointer">
         {{ __($question) }}
     </summary>
