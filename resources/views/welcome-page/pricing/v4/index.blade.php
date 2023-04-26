568158d7dc64befe0ebdf6aa470acb706a54a9e9
Vladyslav Lopatin <vladyslav.lopatin@wskz.pl>
2023-04-26 15:22:35 +0200

    pricing-v4

--- resources/views/welcome-page/pricing/v4/index.blade.php
+++ resources/views/welcome-page/pricing/v4/index.blade.php
@@ -12,13 +12,13 @@
                     <div class="flex flex-col px-9 pt-8 pb-9 h-full bg-white bg-opacity-90 border border-gray-100 rounded-3xl" style="backdrop-filter: blur(46px);">
                         <div>
                             <span class="mb-9 inline-block text-sm {{ config('laravel-welcome-page.text_color_secondary') }} font-semibold uppercase tracking-px leading-snug">
-                                {{ config('spark.billables.user.plans.0.name') }}
+                                {{ __(config('spark.billables.user.plans.0.name')) }}
                             </span>
                             <h3 class="mb-6 text-4xl text-gray-900 font-bold leading-none">
                                 ${{ config('spark.billables.user.plans.0.monthly_price') }}{{ __('laravel-welcome-page::pricing-v4.card_1_per') }}
                             </h3>
                             <p class="mb-9 text-gray-600 font-medium leading-relaxed">
-                                {{ config('spark.billables.user.plans.0.short_description') }}
+                                {{ __(config('spark.billables.user.plans.0.short_description')) }}
                             </p>
                         </div>
                         <a href="{{ config('laravel-welcome-page.pricing_button_path') }}">
@@ -28,8 +28,15 @@
                         </a>
                         <ul class="mt-10">
                             @foreach(config('spark.billables.user.plans.0.features') as $planFeature)
-                                <li class="mb-1.5 text-gray-500 font-medium leading-normal text-center">
-                                    {!! __($planFeature) !!}
+                                <li class="mb-4 flex items-center">
+                                    <span>
+                                        <svg class="mr-2" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M7.5 10L9.16667 11.6667L12.5 8.33333M17.5 10C17.5 14.1421 14.1421 17.5 10 17.5C5.85786 17.5 2.5 14.1421 2.5 10C2.5 5.85786 5.85786 2.5 10 2.5C14.1421 2.5 17.5 5.85786 17.5 10Z" stroke="{{ config('laravel-welcome-page.color_secondary_hex') }}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                                        </svg>
+                                    </span>
+                                    <p class="font-semibold leading-normal">
+                                        {!! __($planFeature) !!}
+                                    </p>
                                 </li>
                             @endforeach
                         </ul>
@@ -44,13 +51,13 @@
 
                         <div class="relative z-10">
                             <span class="mb-9 inline-block text-sm {{ config('laravel-welcome-page.text_color_primary') }} font-semibold uppercase tracking-px leading-snug">
-                                {{ config('spark.billables.user.plans.1.name') }}
+                                {{ __(config('spark.billables.user.plans.1.name')) }}
                             </span>
                             <h3 class="mb-6 text-4xl text-gray-900 font-bold leading-none">
                                 ${{ config('spark.billables.user.plans.1.monthly_price') }}{{ __('laravel-welcome-page::pricing-v4.card_2_per') }}
                             </h3>
                             <p class="mb-9 text-gray-600 font-medium leading-relaxed">
-                                {{ config('spark.billables.user.plans.1.short_description') }}
+                                {{ __(config('spark.billables.user.plans.1.short_description')) }}
                             </p>
                             <a href="{{ config('laravel-welcome-page.pricing_button_path') }}">
                                 <button class="py-4 px-5 w-full text-white font-semibold rounded-xl focus:ring focus:ring-gray-500 bg-black hover:bg-gray-900 transition ease-in-out duration-200 select-none" type="button">
@@ -59,8 +66,15 @@
                             </a>
                             <ul class="mt-10">
                                 @foreach(config('spark.billables.user.plans.1.features') as $planFeature)
-                                    <li class="mb-1.5 text-gray-500 font-medium leading-normal text-center">
-                                        {!! __($planFeature) !!}
+                                    <li class="mb-4 flex items-center">
+                                    <span>
+                                        <svg class="mr-2" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M7.5 10L9.16667 11.6667L12.5 8.33333M17.5 10C17.5 14.1421 14.1421 17.5 10 17.5C5.85786 17.5 2.5 14.1421 2.5 10C2.5 5.85786 5.85786 2.5 10 2.5C14.1421 2.5 17.5 5.85786 17.5 10Z" stroke="{{ config('laravel-welcome-page.color_secondary_hex') }}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                                        </svg>
+                                    </span>
+                                        <p class="font-semibold leading-normal">
+                                            {!! __($planFeature) !!}
+                                        </p>
                                     </li>
                                 @endforeach
                             </ul>
@@ -71,13 +85,13 @@
                     <div class="flex flex-col px-9 pt-8 pb-9 h-full bg-white bg-opacity-90 border border-gray-100 rounded-3xl" style="backdrop-filter: blur(46px);">
                         <div>
                             <span class="mb-9 inline-block text-sm {{ config('laravel-welcome-page.text_color_secondary') }} font-semibold uppercase tracking-px leading-snug">
-                                {{ config('spark.billables.user.plans.2.name') }}
+                                {{ __(config('spark.billables.user.plans.2.name')) }}
                             </span>
                             <h3 class="mb-6 text-4xl text-gray-900 font-bold leading-none">
                                 ${{ config('spark.billables.user.plans.2.monthly_price') }}{{ __('laravel-welcome-page::pricing-v4.card_3_per') }}
                             </h3>
                             <p class="mb-9 text-gray-600 font-medium leading-relaxed">
-                                {{ config('spark.billables.user.plans.2.short_description') }}
+                                {{ __(config('spark.billables.user.plans.2.short_description')) }}
                             </p>
                         </div>
                         <a href="{{ config('laravel-welcome-page.pricing_button_path') }}">
@@ -87,8 +101,15 @@
                         </a>
                         <ul class="mt-10">
                             @foreach(config('spark.billables.user.plans.2.features') as $planFeature)
-                                <li class="mb-1.5 text-gray-500 font-medium leading-normal text-center">
-                                    {!! __($planFeature) !!}
+                                <li class="mb-4 flex items-center">
+                                    <span>
+                                        <svg class="mr-2" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M7.5 10L9.16667 11.6667L12.5 8.33333M17.5 10C17.5 14.1421 14.1421 17.5 10 17.5C5.85786 17.5 2.5 14.1421 2.5 10C2.5 5.85786 5.85786 2.5 10 2.5C14.1421 2.5 17.5 5.85786 17.5 10Z" stroke="{{ config('laravel-welcome-page.color_secondary_hex') }}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                                        </svg>
+                                    </span>
+                                    <p class="font-semibold leading-normal">
+                                        {!! __($planFeature) !!}
+                                    </p>
                                 </li>
                             @endforeach
                         </ul>
