0f69d704f2d4cdfc975c90c2c52e02e369b631bf
Vladyslav Lopatin <vladyslav.lopatin@wskz.pl>
2023-08-28 14:26:10 +0200

    web middleware

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['web'])->group(function () {
+Route::domain(env('APP_URL'))->middleware(['web'])->group(function () {
     Route::get('/', [\Atin\LaravelWelcomePage\Http\Controllers\WelcomePageController::class, 'index'])
         ->name('home');
 });
