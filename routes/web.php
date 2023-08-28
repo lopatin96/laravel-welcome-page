15a8f72396728a488edddaaef9f60e236e722ad8
Vladyslav Lopatin <vladyslav.lopatin@wskz.pl>
2023-08-28 12:32:53 +0200

    web middleware

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [\Atin\LaravelWelcomePage\Http\Controllers\WelcomePageController::class, 'index'])
-    ->name('home');
+Route::middleware(['web'])->group(function () {
+    Route::get('/', [\Atin\LaravelWelcomePage\Http\Controllers\WelcomePageController::class, 'index'])
+        ->name('home');
+});
