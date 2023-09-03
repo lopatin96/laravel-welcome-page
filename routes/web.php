c2a483ca780916228133e9a744e1e349c4f85d2b
vladyslavlopatin <ilopatin3@gmail.com>
2023-09-03 12:55:54 +0200

    fix route

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::domain(env('APP_URL'))->middleware(['web'])->group(function () {
-    Route::get('/', [\Atin\LaravelWelcomePage\Http\Controllers\WelcomePageController::class, 'index'])
-        ->name('home');
-});
+Route::get('/', [\Atin\LaravelWelcomePage\Http\Controllers\WelcomePageController::class, 'index'])
+    ->name('home');
